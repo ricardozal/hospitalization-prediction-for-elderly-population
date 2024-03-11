a0a877fba9004218cb9fd74f95709f435978583d
Ricardo Zaldívar Pichardo <ricardozalpich@gmail.com>
2024-03-11 17:58:55 -0600

    Refactor src/data_utils.py

--- src/data_utils.py
+++ src/data_utils.py
@@ -1,11 +1,10 @@
 import os
-from typing import List, Tuple
-from collections import OrderedDict
+from typing import List
 
 import gdown
 import pandas as pd
 import numpy as np
-from pandas.io.sas.sasreader import ReaderBase
+import re
 
 from src import config
 from src.section_dict import (
@@ -26,6 +25,10 @@ from src.section_dict import (
     section_Q  # Q: PSYCHOSOCIAL
 )
 
+sections = [section_A, section_B, section_C, section_D, section_E, section_F,
+            section_G, section_H, section_I, section_J, section_K, section_L,
+            section_M, section_O, section_Q]
+
 def get_datasets() -> pd.DataFrame:
     """
     Download from GDrive all the needed datasets for the project.
@@ -40,14 +43,6 @@ def get_datasets() -> pd.DataFrame:
         gdown.download(
             config.DATASET_MHAS_URL, config.DATASET_MHAS_C2, quiet=False
         )
-
-    # Download H_MHAS_EOL_b.sas7bdat
-    if not os.path.exists(config.DATASET_MHAS_EOL):
-        gdown.download(config.DATASET_MHAS_EOL_URL, config.DATASET_MHAS_EOL, quiet=False)
-
-    # Download H_MEX_COG_A2_2016.dta
-    if not os.path.exists(config.DATASET_MEX_COG):
-        gdown.download(config.DATASET_MEX_COG_URL, config.DATASET_MEX_COG, quiet=False)
 
     # read the dataset H_MHAS_c2 from the file (470 MB)
     file_path = './dataset/H_MHAS_c2.sas7bdat'
@@ -66,99 +61,65 @@ def get_datatrain() -> pd.DataFrame:
     
     return app_train
 
-def outline_dataframe(app_all: pd.DataFrame, elimins: List[list]) -> pd.DataFrame:
-    sections = [section_A, section_B, section_C, section_D, section_E, section_F,
-                section_G, section_H, section_I, section_J, section_K, section_L,
-                section_M, section_O, section_Q]
-    
-    working_app_all = app_all.copy()
 
-    # Define old and new column names
-    old_dfnames = ['hh1ctot1m', 'hh1cftot1m', 'hh2cftot1m', 'hh2ctot1m', 'hh3cftot1m', 'hh3ctot1m', 'hh4cftot1m', 'hh4ctot1m', 'hh5cftot1m', 'hh5ctot1m']
-    new_dfnames = ['h1ctot1m', 'h1cftot1m', 'h2cftot1m', 'h2ctot1m', 'h3cftot1m', 'h3ctot1m', 'h4cftot1m', 'h4ctot1m', 'h5cftot1m', 'h5ctot1m']
-    # Rename columns
-    working_app_all.rename(columns=dict(zip(old_dfnames, new_dfnames)), inplace=True)
+def remove_unnecessary_columns(df: pd.DataFrame, subsections_to_remove: List):
+    features_to_remove = []
+    for section in sections:
+        for subsection, features in section.items():
+            if subsection in subsections_to_remove:
+                features_to_remove += features
 
-    waves_col = filter_dataset(sections, elimins)
+    df = df.drop(columns=features_to_remove)
 
-    waves = []
+    return df
 
-    for i in range(5):  
-        data = {col: working_app_all[col] if col in working_app_all.columns else np.nan for col in waves_col[i]}
-        wave = pd.DataFrame(data)
-        wave.columns = [item[0] + 'X' + item[2:] if item[1].isdigit() else item for item in wave.columns.tolist()]
-        waves.append(wave)
-        
-    app_all_data = pd.concat(waves)
 
-    return app_all_data
+def concat_waves(df: pd.DataFrame):
+    column_names_new = set([re.sub(r'^(r|s|h|hh)\d', '', x) for x in df.columns])
+    print(column_names_new)
+    print(f'TAMAÑO = {len(column_names_new)}')
+    dfs = {key: list() for key in column_names_new}
 
-def filter_dataset(sections: List[dict], elimins: List[list]) -> Tuple[List[str]]:
-    """
-    Splits the entire column list of the DataFrame into five different interview columns.
+    # Dictionary to hold each column as a separate DataFrame
+    column_dfs = {}
 
-    Returns:
-        waves : List[list]
-                  
-    """
-    
-    Total_sections = []
+    # Split each column into a separate DataFrame
+    for column in df:
+        column_dfs[column] = df[[column]]
 
-    # Iterate over each dictionary and its values
-    for section, elimin in zip(sections, elimins):
-        filtered_section = {k: v for k, v in section.items() if k not in elimin}
-        # Extend the Total_sections list with the values from the current list
-        for values_list in filtered_section.values():
-            Total_sections.extend(values_list)
+    for key, value in column_dfs.items():
+        for col_new in column_names_new:
+            pattern = re.compile(r'\d' + col_new + '$')
+            if bool(pattern.search(key)):
+                print(f'from {value.columns[0]} to {col_new}')
+                value = value.rename(columns={value.columns[0]: col_new})
+                dfs[col_new].append(value)
+            elif key == col_new:
+                print(f'from {value.columns[0]} to {col_new}')
+                dfs[col_new].append(value)
 
-    new_list = [item[0] + 'X' + item[2:] if item[1].isdigit() else item for item in Total_sections]
 
-    unique_values_array = np.unique(new_list)
-    # No need to convert to list
-    # Five different lists were created to store the information from the five interviews.
-    waves = [[s.replace('X', str(i)) for s in unique_values_array] for i in range(1, 6)]
+    for key, value in dfs.items():
+        print(f'{key} was resized to 1 from {len(value)}')
 
-    return tuple(waves)
 
-def unify_spousandref(app_train: pd.DataFrame)-> pd.DataFrame:
-    # Extract all column names from the dataframe into a names_data_app list.
-    names_data_app = app_train.columns.tolist()
+    # Adjusted step to concatenate DataFrames within each list and reset index
+    concat_dfs = {key: pd.concat(df_list, ignore_index=True, axis=0) if df_list else pd.DataFrame() for key, df_list in dfs.items()}
 
-    # Divide the list into three different lists: names_starting_s, names_starting_r, names_starting_other
-    names_starting_s = [name for name in names_data_app if name.startswith('s')]
-    names_starting_r = [name for name in names_data_app if name.startswith('r')]
-    names_starting_other = [name for name in names_data_app if not name.startswith('s') and not name.startswith('r')]
+    # Merge all concatenated DataFrames side by side
+    final_df = pd.concat(concat_dfs.values(), axis=1)
 
-    # Make 2 new DataFrames
-    app_train_r = app_train[names_starting_other + names_starting_r]
-    app_train_s = app_train[names_starting_other + names_starting_s]
+    for col_new in column_names_new:
+        if len(dfs[col_new]) == 1:
+            print(f'{col_new} was filled')
+            unique_values = final_df.loc[final_df[col_new].notna(), col_new].unique()
 
-    # Check if the column name starts with 's' before replacing the first letter
-    app_train_s.columns = [name.replace('s', 'r', 1) if name.startswith('s') else name for name in app_train_s.columns]
+            # Calculate the number of NaNs to fill
+            nan_count = final_df[col_new].isna().sum()
 
-    vars_in_s = ['rXbmonth', 'rXbyear', 'rXdmonth', 'rXdyear', 'rXedisced', 'rXeducl', 'rXedyrs', 'rXevbrn', 'rXfeduc_m', 'rXgender', 'rXindlang', 'rXliterate', 'rXmeduc_m', 'rXnumerate']
-    # New variable names
-    vars_to_repl = ['rabmonth', 'rabyear', 'radmonth', 'radyear', 'raedisced', 'raeducl', 'raedyrs', 'raevbrn', 'rafeduc_m', 'ragender', 'raindlang', 'raliterate', 'rameduc_m', 'ranumerate']
-    
-    # Create a mapping between the old variable names and the new ones
-    variable_mapping = {old_var: new_var for old_var, new_var in zip(vars_in_s, vars_to_repl)}
-    
-    # Replace the variable names in vars_in_s with the new ones
-    app_train_s.columns = [variable_mapping.get(var, var) for var in app_train_s.columns]
-    
-    # Concatenate the 2 new DataFrames into one DataFrame app_train_sr
-    app_train_sr = pd.concat([app_train_r, app_train_s], axis=0)
-    
-    # Identify the variable named 'rXhosp1y' and rename it to 'Target'
-    app_train_sr.rename(columns={'rXhosp1y': 'TARGET'}, inplace=True)
+            sequence_to_fill = np.resize(unique_values, nan_count)
 
-    # Delete all rows where the variable 'Target' is equal to null
-    app_train_sr.dropna(subset=['TARGET'], inplace=True)
+            # Assign this sequence to the NaN positions in each column
+            final_df.loc[final_df[col_new].isna(), col_new] = sequence_to_fill
 
-    # Rename the DataFrame as app_train
-    app_train = app_train_sr.copy()
-
-    # reset the index after dropping the rows with null 'TARGET'
-    app_train.reset_index(drop=True, inplace=True)
-       
-    return app_train
+    return final_df
