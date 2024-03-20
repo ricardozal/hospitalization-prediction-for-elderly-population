5b6fd34acad823c7229063529c8f5905dbb5c2b8
Ricardo Zaldívar Pichardo <ricardozalpich@gmail.com>
2024-03-20 17:18:57 -0600

    create training model script

--- src/constants.py
+++ src/constants.py
@@ -1,31 +1,28 @@
+EXCLUDED_SUBSECTIONS = ['Person Specific Identifier', 'Household Identifier', 'Spouse Identifier',
+                              'Response Indicator', 'Sample Cohort', 'Whether Proxy Interview',
+                              'Household Analysis Weight', 'Person-Level Analysis Weight', 'Death Date',
+                              'Literacy and Numeracy', 'ADL Summary', 'IADL Summary', 'Sleep', 'Menopause',
+                              'Preventive Care', 'Medical Expenditures', 'Covered by Government', 'Covered by Private',
+                              'Covered by Employer', 'Date Naming/Orientation', 'JORM IQCODE', 'Inflation Multiplier',
+                              'Net Value of Real Estate', 'Net Value of Cars', 'Net Value of Businesses',
+                              'Value of Stocks', 'Value of Accounts', 'Value of Other', 'Value of Residence',
+                              'Value of Mortgages', 'Net Value P Residence', 'Home ownership', 'Value of Other Debt',
+                              'Value of Loans Lent', 'Net Value Financial Wealth', 'Individual Earnings',
+                              'Household Income', 'Income from Private Pension', 'Public Pension Income',
+                              'Other Pensions Income', 'Total Pensions Income', 'Other Government Transfers',
+                              'All Other Income', 'Weekly Social Activities', 'Currently Working for Pay',
+                              'Whether Self-Employed', 'Labor Force Status', 'In the Labor Force',
+                              'Retired Employment Status', 'Hours at Main Job', 'Main Activity Years of Tenure',
+                              'Job Allows Move', 'Occupation Code with Longest', 'Retirement year', 'Public Pension',
+                              'Private Pension', 'Other Pension', 'Age Public Pension', 'Age Private Pension',
+                              'Public Pension Continuity', 'Private Pension Continuity', 'Sitting Height',
+                              'Sitting Height Incomplete', 'Blood Pressure Measure', 'Blood Pressure Measure Incomplete',
+                              'Timed Walk Measure', 'Timed Walk Measure Incomplete', 'Hand Grip Measure',
+                              'Hand Grip Measure Incomplete', 'Has a Will', 'Beneficiaries Will',
+                              'Covered Life Insurance', 'Cantril Ladder']
 
-UNNECESARY_SECTION_COLUMNS = [
-    "Person Specific Identifier", "Household Identifier",
-    "Spouse Identifier", "Response Indicator", "Sample Cohort",
-    "Whether Proxy Interview", "Household Analysis Weight",
-    "Person-Level Analysis Weight", "Death Date", "Literacy and Numeracy",
-    "Preventive Care", "IADL Summary", "Menopause", "Sleep", "ADL Summary",
-    "Covered by Employer", "Covered by Private", "Medical Expenditures",
-    "Covered by Government", "JORM IQCODE", "Date Naming/Orientation",
-    "Inflation Multiplier", "Net Value of Cars", "Value of Loans Lent",
-    "Net Value Financial Wealth", "Home ownership", "Net Value P Residence",
-    "Value of Other Debt", "Value of Accounts", "Net Value of Businesses",
-    "Net Value of Real Estate", "Value of Mortgages", "Value of Other",
-    "Value of Residence", "Value of Stocks", "Public Pension Income",
-    "Household Income", "Income from Private Pension",
-    "Other Pensions Income", "Individual Earnings",
-    "Total Pensions Income", "All Other Income",
-    "Other Government Transfers", "Weekly Social Activities",
-    "Whether Self-Employed", "Main Activity Years of Tenure",
-    "In the Labor Force", "Hours at Main Job", "Job Allows Move",
-    "Retired Employment Status", "Labor Force Status",
-    "Currently Working for Pay", "Occupation Code with Longest",
-    "Retirement year", "Private Pension", "Age Private Pension",
-    "Public Pension Continuity", "Private Pension Continuity",
-    "Age Public Pension", "Public Pension", "Other Pension",
-    "Timed Walk Measure Incomplete", "Timed Walk Measure",
-    "Sitting Height Incomplete", "Sitting Height", "Hand Grip Measure",
-    "Blood Pressure Measure Incomplete", "Blood Pressure Measure",
-    "Hand Grip Measure Incomplete", "Beneficiaries Will",
-    "Covered Life Insurance", "Has a Will", "Cantril Ladder"
-]
+USELESS_FEATURES = ['rabyear', 'rabmonth', 'iwy', 'iwm', 'hspnit1y', 'doctim1y', 'momage', 'dadage']
+
+ORIGINAL_TARGET_NAME = 'hosp1y'
+
+FINAL_MODEL = 'final_model.pkl'
